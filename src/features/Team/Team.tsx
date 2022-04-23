e462324a310732efd47d8985f5021fd64fa09206
Pavel <Pikcelll@yandex-team.ru>
2022-04-23 23:15:54 +0300

    deploy

--- src/features/Team/Team.tsx
+++ src/features/Team/Team.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import classes from "./Team.module.scss";
 import pavel from "../../shared/icons/pavel.png";
 import rustam from "../../shared/icons/rustam.png";
+import lenar from "../../shared/icons/lenar.png";
 import keys from "../../shared/keys/keys.json";
 
 type TeamProps = {
@@ -30,6 +31,15 @@ const Team: FC<TeamProps> = ({ lang }) => {
           </div>
           <div className={classes.work}>
             {keys.common.team.rustam.work[lang]}
+          </div>
+        </div>
+        <div className={classes.card}>
+          <img src={lenar} alt="" className={classes.img} />
+          <div className={classes.name}>
+            {keys.common.team.lenar.name[lang]}
+          </div>
+          <div className={classes.work}>
+            {keys.common.team.lenar.work[lang]}
           </div>
         </div>
       </div>
